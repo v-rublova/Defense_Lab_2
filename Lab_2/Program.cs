1198502d085dc94b08ee422a13b6e734dbb24a7c
v.rublova <v.rublova@student.csn.khai.edu>
2020-09-21 18:41:42 +0300

    Code and decode algorithms for Gronsfeld cipher are implemented

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -7,86 +7,59 @@ namespace Lab_2
     static class Alphabets
     {
         public static List<char> alph;
+        public static List<char> alph_ukr;
 
     }
-    public static class Bruteforce
+    public static class Gronsfeld_Cipher
     {
-        public static void FillUp(ref List<List<char>> table, string message, List<char> alphabet)
+        public static string Code(string message, string key,List<char> alphabet)
         {
-
-            InitalizeList(ref table, message.Length);
-            int index = 0;
+            string coded = "";
+            int key_iterator = 0;
             for (int i = 0; i < message.Length; i++)
             {
-                index = alphabet.IndexOf(message[i]);
-                for (int j = 0; j < 10; j++)
+                if (key_iterator >= key.Length) key_iterator = 0;
+                int shift = Convert.ToInt32(key.Substring(key_iterator,1));
+                int distance = alphabet.IndexOf(message[i]) - shift;
+                if (distance < 0)
                 {
-                    if (index >= alphabet.Count)
-                    {
-                        index = 0;
-                    }
-                    table[i].Add(alphabet[index]);
-                    index++;
+                    distance = alphabet.Count - shift;
                 }
+                coded += alphabet[distance];
+                key_iterator++;
             }
+            return coded;
         }
-        public static void BruteKey(ref List<string> res, List<List<char>> table, string message, int mul)
-        { 
-            int limit = 1;
-            for(int i = 0; i < mul; i++)
-            {
-                limit *= 10;
-            }
-            for (int key_size = 0; key_size < limit; key_size++)
-            {
-                string pos = "";
-                string key = "";
-                key = key_size.ToString().PadLeft(mul - 1, '0');
-                int h = 0;//key iterator
-                for (int g = 0; g < message.Length; g++)
-                {
-                    if (h + 1 > key.Length)
-                        h = 0;
-                    pos += table[g][Convert.ToInt32((key.Substring(h, 1)))];
-                    h++;
-                }
-                res.Add(pos);
-                Console.WriteLine(pos);
-            }
-
-        }
-        static void InitalizeList(ref List<List<char>> list, int size)
+        public static string Decode(string coded_message,string key,List<char> alphabet)
         {
-            for (int i = 0; i < size; i++)
+            string decoded = "";
+            int key_iterator = 0;
+            for (int i = 0; i < coded_message.Length; i++)
             {
-                list.Add(new List<char>());
+                if (key_iterator >= key.Length) key_iterator = 0;
+                int shift = Convert.ToInt32(key.Substring(key_iterator, 1));
+                int distance = alphabet.IndexOf(coded_message[i]) + shift;
+                if (distance >= alphabet.Count)
+                {
+                    distance = alphabet.Count + shift;
+                }
+                decoded += alphabet[distance];
+                key_iterator++;
             }
+            return decoded;
         }
     }
+    
     class Program
     {
         static void Main(string[] args)
         {
-
-            string coded_m_1 = "трхшуеокйефхмптпдцсрхшхзчкм";
-            string coded_m_2 = "сжрссчпббдуусхрогдожзенй";
-            string test = "фпжисьиоссахилфиусс";
-
             InitializeAlphabet();
-            List<string> bruted = new List<string>();
-            List<List<char>> table = new List<List<char>>();
+            string message_1 = "зустріч призначена в сім";
 
-            Bruteforce.FillUp(ref table, coded_m_2, Alphabets.alph);    
-            Bruteforce.BruteKey(ref bruted, table, coded_m_2, 4);//where 6 - assumed key length
 
-            using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter(@"E:\3 course (inst)\Defence X\bruted_full_2_4.txt"))
-            {
-                foreach (string s in bruted)
-                {
-                    file.WriteLine(s, Encoding.ASCII);
-                }
-            }
+            Console.WriteLine(Gronsfeld_Cipher.Code(message_1, "1601", Alphabets.alph_ukr));
+            Console.WriteLine(Gronsfeld_Cipher.Decode(Gronsfeld_Cipher.Code(message_1, "1601", Alphabets.alph_ukr),"1601",Alphabets.alph_ukr));
             Console.ReadKey();
         }
         static void InitializeAlphabet()
@@ -126,7 +99,44 @@ namespace Lab_2
             'б',
             'а',
             ' '
-            };   
+            };
+            Alphabets.alph_ukr = new List<char>
+            {
+            'я',
+            'ю',         
+            'ь',    
+            'щ',
+            'ш',
+            'ч',
+            'ц',
+            'х',
+            'ф',
+            'у',
+            'т',
+            'с',
+            'р',
+            'п',
+            'о',
+            'н',
+            'м',
+            'л',
+            'к',
+            'й',
+            'ї',
+            'і',
+            'и',
+            'з',
+            'ж',
+            'є',
+            'е',
+            'д',
+            'ґ',
+            'г',
+            'в',
+            'б',
+            'а',
+            ' '
+            };
         }
     }
 }
