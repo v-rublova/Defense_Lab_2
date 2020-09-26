d14d98b0a99300cf10cc5dcc8e4953b82187a28b
v.rublova <v.rublova@student.csn.khai.edu>
2020-09-26 12:56:59 +0300

    Added numeric decoder

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -8,18 +8,19 @@ namespace Lab_2
     {
         public static List<char> alph;
         public static List<char> alph_ukr;
+        public static List<byte> alph_numbers;
 
     }
     public static class Gronsfeld_Cipher
     {
-        public static string Code(string message, string key,List<char> alphabet)
+        public static string Code(string message, string key, List<char> alphabet)
         {
             string coded = "";
             int key_iterator = 0;
             for (int i = 0; i < message.Length; i++)
             {
                 if (key_iterator >= key.Length) key_iterator = 0;
-                int shift = Convert.ToInt32(key.Substring(key_iterator,1));
+                int shift = Convert.ToInt32(key.Substring(key_iterator, 1));
                 int distance = alphabet.IndexOf(message[i]) - shift;
                 if (distance < 0)
                 {
@@ -30,7 +31,7 @@ namespace Lab_2
             }
             return coded;
         }
-        public static string Decode(string coded_message,string key,List<char> alphabet)
+        public static string Decode(string coded_message, string key, List<char> alphabet)
         {
             string decoded = "";
             int key_iterator = 0;
@@ -41,7 +42,25 @@ namespace Lab_2
                 int distance = alphabet.IndexOf(coded_message[i]) + shift;
                 if (distance >= alphabet.Count)
                 {
-                    distance = alphabet.Count + shift;
+                    distance = distance - alphabet.Count - 1;
+                }
+                decoded += alphabet[distance];
+                key_iterator++;
+            }
+            return decoded;
+        }
+        public static string Decode(List<byte> coded_message, List<byte> key, List<byte> alphabet)
+        {
+            string decoded = "";
+            int key_iterator = 0;
+            for (int i = 0; i < coded_message.Count; i++)
+            {
+                if (key_iterator >= key.Count) key_iterator = 0;
+                int shift = key[key_iterator];
+                int distance = alphabet.IndexOf(coded_message[i]) + shift;
+                if (distance >= alphabet.Count)
+                {
+                    distance = distance - alphabet.Count-1;
                 }
                 decoded += alphabet[distance];
                 key_iterator++;
@@ -49,21 +68,23 @@ namespace Lab_2
             return decoded;
         }
     }
-    
+
     class Program
     {
         static void Main(string[] args)
         {
             InitializeAlphabet();
             string message_1 = "зустріч призначена в сім";
-
+            string message_2 = "у судді проникливий розум;";
+            string message_3 = "спеціальний підрозділ";
 
             Console.WriteLine(Gronsfeld_Cipher.Code(message_1, "1601", Alphabets.alph_ukr));
-            Console.WriteLine(Gronsfeld_Cipher.Decode(Gronsfeld_Cipher.Code(message_1, "1601", Alphabets.alph_ukr),"1601",Alphabets.alph_ukr));
+            Console.WriteLine(Gronsfeld_Cipher.Decode(Gronsfeld_Cipher.Code(message_1, "1601", Alphabets.alph_ukr), "1601", Alphabets.alph_ukr));
+            Console.WriteLine(Gronsfeld_Cipher.Decode(new List<byte> { 4, 6, 6, 8, 4, 6 }, new List<byte> { 1, 2, 8 }, Alphabets.alph_numbers));
             Console.ReadKey();
         }
         static void InitializeAlphabet()
-        {   
+        {
             Alphabets.alph = new List<char>
             {
             'я',
@@ -103,8 +124,8 @@ namespace Lab_2
             Alphabets.alph_ukr = new List<char>
             {
             'я',
-            'ю',         
-            'ь',    
+            'ю',
+            'ь',
             'щ',
             'ш',
             'ч',
@@ -136,6 +157,10 @@ namespace Lab_2
             'б',
             'а',
             ' '
+            };
+            Alphabets.alph_numbers = new List<byte>
+            {
+                9,8,7,6,5,4,3,2,1,0
             };
         }
     }
